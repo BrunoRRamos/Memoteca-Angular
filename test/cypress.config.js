fc43cf52ec1baf753cdd8c841a0086c71c3b0b13
Bruno Ramos <bruno.ramos.ext@synchro.com.br>
2025-02-24 14:48:23 -0300

    refactor: Ajustando função e modularizando como plugin

--- test/cypress.config.js
+++ test/cypress.config.js
@@ -1,6 +1,4 @@
-const fs = require("fs");
-const path = require("path");
-const XLSX = require("xlsx");
+const { convertXlsxToJson } = require("./cypress/plugins");
 
 module.exports = {
   viewportWidth: 1280,
@@ -15,20 +13,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       on("task", {
         readXlsx({ fileName }) {
-          try {
-            const filePath = path.join(__dirname, "cypress", "fixtures", `${fileName}.xlsx`);
-            const buffer = fs.readFileSync(filePath);
-            const workbook = XLSX.read(buffer, { type: "buffer" });
-            const sheetName = workbook.SheetNames[0];
-            const sheet = workbook.Sheets[sheetName];
-
-            const jsonData = XLSX.utils.sheet_to_json(sheet, { defval: "" });
-
-            return jsonData[0];
-          } catch (error) {
-            console.error("Erro ao converter o arquivo:", error);
-            return null;
-          }
+         return convertXlsxToJson(fileName);
         },
       });
     },
---
+++ test/cypress/plugins/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const XLSX = require("xlsx");
+
+function convertXlsxToJson (fileName) {
+    try {
+        const filePath = path.join(__dirname, "..", "fixtures", `${fileName}.xlsx`);
+        const buffer = fs.readFileSync(filePath);
+        const workbook = XLSX.read(buffer, { type: "buffer" });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { defval: "" });
+
+        return jsonData[0];
+    } catch (error) {
+        console.log("Erro ao converter o arquivo:", error);
+        return null;
+    }
+}
+
+module.exports = {
+    convertXlsxToJson,
+}
